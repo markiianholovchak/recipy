d829d645eff8f32f1ea17204557e0a267e548023
markiian.holovchak <markiianholovchak@gmail.com>
2023-11-11 17:23:51 +0100

    Created homepage header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,34 +6,24 @@ export const Footer = () => {
         <footer className="mb-16 mt-10">
             <ContentContainer>
                 <div className="flex flex-col gap-4">
-                    <div className="flex justify-between">
+                    <div className="flex flex-col justify-between gap-4 md:flex-row">
                         <div className="flex flex-col gap-2">
                             <p className="text-2xl font-semibold">Recipy</p>
-                            <ul className="flex gap-2">
-                                <li>
-                                    <a href="#">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="#">Contact</a>
-                                </li>
-                                <li>
-                                    <a href="#">LinkTree</a>
-                                </li>
-                            </ul>
+                            <nav className="flex gap-2">
+                                <a href="#">About Us</a>
+                                <a href="#">Contact</a>
+                                <a href="#">LinkTree</a>
+                            </nav>
                         </div>
                         <NewsLetterForm />
                     </div>
                     <hr className="border-black" />
                     <div className="flex justify-between font-light text-gray-500">
                         <p>&copy; {new Date().getFullYear()} Recipy. All Rights Reserved.</p>
-                        <ul className="flex gap-2">
-                            <li>
-                                <a href="#">Privacy</a>
-                            </li>
-                            <li>
-                                <a href="#">Terms of Use</a>
-                            </li>
-                        </ul>
+                        <nav className="flex gap-2">
+                            <a href="#">Privacy</a>
+                            <a href="#">Terms of Use</a>
+                        </nav>
                     </div>
                 </div>
             </ContentContainer>
